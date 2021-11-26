20cc5e6efc42ee0f191a737ba79287c7055fbec7
BangDC <polopalayyy@gmail.com>
2021-11-26 09:15:04 +0700

    delete unnecessary files

--- hub/Models/ErrorModal.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Signal.Models
-{
-    public class ErrorModal
-    {
-        public string RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
